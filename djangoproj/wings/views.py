bec729b3979dcfae2e0a7e846aa2acad73af292e
justineclaire <langridge.justine@gmail.com>
2022-04-21 00:48:02 +0100

    bootstrap and static files

--- djangoproj/wings/views.py
+++ djangoproj/wings/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
 from django.utils.translation import gettext_lazy as _
 
 
 def testlang(request):
     return HttpResponse(_('Welcome to language translation!'))
+
+def index(request):
+    return render(request, "index.html")
